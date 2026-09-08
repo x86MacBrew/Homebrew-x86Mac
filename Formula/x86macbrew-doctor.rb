ebcb0df1d34fb346b9fe8a8df33b8ea04187e3e6
Aarush Jain <aarushjain1907@gmail.com>
2026-09-08 19:14:43 -0400

    Add x86macbrew-doctor formula

--- Formula/x86macbrew-doctor.rb
+++ Formula/x86macbrew-doctor.rb
@@ -1,9 +1,9 @@
 class X86macbrewDoctor < Formula
   desc "Diagnose whether an Intel Mac meets x86MacBrew's support contract"
   homepage "https://github.com/x86MacBrew/homebrew-x86mac"
-  url "__RELEASE_URL__"
-  version "__VERSION__"
-  sha256 "__SHA256__"
+  url "https://github.com/x86MacBrew/Homebrew-x86Mac/releases/download/v0.1.0/x86macbrew-doctor-0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "86fd451c72ff4691eceb0c683fcb6aa39dedc9b5107820c03ddd8150591515fb"
   license "BSD-2-Clause"
 
   def install
